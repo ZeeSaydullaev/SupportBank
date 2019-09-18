0d15abcd0be72e2ffcb7372b64398801bce5521e
Zee <zee.saydullaev@aexp.com>
2019-09-18 14:40:11 +0100

    Basic layout

--- src/main/java/training/supportbank/Main.java
+++ src/main/java/training/supportbank/Main.java
@@ -1,8 +1,60 @@
 package training.supportbank;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String args[]) {
         // Your code here!
         System.out.println("Test!");
     }
+
+
+
+    class TeamMember {
+
+
+
+        String Name;
+
+        BigDecimal Balance;
+
+        }
+
+
+
+
+
+
+
+
+
+
+    class Account {
+
+
+        LocalDate date;
+
+        String narrative;
+
+        BigDecimal sent;
+
+        BigDecimal received;
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+*/
+
 }
